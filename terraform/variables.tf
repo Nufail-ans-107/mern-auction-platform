205a8a5885a6b0e5d4976c288f29a08bb4556864
Nufail-ans-107 <ansarinufail@gmail.com>
2025-08-08 00:46:49 +0530

    add: terraform variable declarations for GitHub Actions

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  type = string
+}
+
+variable "my_ip" {
+  type = string
+}
+
+variable "ami_id" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "key_name" {
+  type = string
+}
